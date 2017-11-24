d841a9a2682465b11891874367b84455ba438bde
Jack Turner <jack@turner.io>
2017-11-24 00:08:51 -0500

    Form now submits to google sheet
    Fixes #3 and submits data to this Google Sheet:
    https://docs.google.com/spreadsheets/d/1P1WbdUmjkliXsqVjve7VkKLVXpFwrx5exWrDoB6F078/edit?usp=sharing
    
    …also added <title/> for page.

---
+++ voice.js
@@ -0,0 +1,9 @@
+$('button').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: 'https://script.google.com/a/turner.io/macros/s/AKfycby9oN0_8u43R7i7SQx-VK6QDIqCe0zzgA-kFvABuQiINdRiOH0/exec',
+    method: 'get',
+    dataType: 'json',
+    data: { protest: $('#protest').val() }
+  }).success(function(response){ console.log(response); })
+})
