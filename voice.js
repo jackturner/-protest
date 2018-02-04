7427da365b5c5e7d099c82706082af6f25154715
Jack Turner <jack@turner.io>
2018-02-04 15:31:54 -0500

    Response shown when submitting protest; CSS tweaks
    Distance of the textarea from the top of the page is reduced to show more of the text area. Also made the textarea less tall.
    
    Now shows a confirmation message when protest has been submitted.

--- voice.js
+++ voice.js
@@ -6,13 +6,21 @@ $('.info-link').on('click', function(e) {
 
 $('button').on('click', function(e) {
   e.preventDefault()
+  var protest = $('#protest').val()
+  if (protest === "") return
+  $(this).prop('disabled', true)
   var jqxhr = $.ajax({
     url: 'https://script.google.com/a/turner.io/macros/s/AKfycby9oN0_8u43R7i7SQx-VK6QDIqCe0zzgA-kFvABuQiINdRiOH0/exec',
     method: 'get',
     dataType: 'json',
-    data: { protest: $('#protest').val() }
-  }).success(function(response){ console.log(response) })
+    data: { protest: protest }
+  }).success(successful_protest)
 })
+
+var successful_protest = function (response) {
+  console.log(response)
+  $('form').addClass('disabled')
+}
 
 $(window).on('scroll', function() {
 	$('.dim').css('opacity', (Math.min(0.6, (window.scrollY / $(window).height() / 2) + 0.3)))
